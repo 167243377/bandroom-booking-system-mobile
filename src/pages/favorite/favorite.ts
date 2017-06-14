7da12b53132cdd73828ddad2ec73fcf7311488e1
167243377 <167243377@stu.vtc.edu.hk>
2017-06-14 23:40:20 +0800

    favorite updated

--- src/pages/favorite/favorite.ts
+++ src/pages/favorite/favorite.ts
@@ -1,44 +1,82 @@
+import { AppSettings } from '../../appSettings';
 import { Component } from '@angular/core';
-import { NavController, NavParams, MenuController, ModalController } from 'ionic-angular';
+import { NavController, NavParams, MenuController, ModalController, AlertController } from 'ionic-angular';
 import { RoomService } from "../../services/roomService";
 import { Room } from "../../model/room";
 import { RoomPage } from "../room/room";
 
-/*
- Generated class for the Favorite page.
+import { Storage } from '@ionic/storage';
 
- See http://ionicframework.com/docs/v2/components/#navigation for more info on
- Ionic pages and navigation.
- */
 @Component({
-  selector: 'page-favorite',
-  templateUrl: 'favorite.html'
+    selector: 'page-favorite',
+    templateUrl: 'favorite.html'
 })
 export class FavoritePage {
-  private favoriteRooms;
+    private host = AppSettings.apiHost;
+    private favoriteRooms = [];
 
-  constructor(private navCtrl: NavController,
-    private roomsService: RoomService,
-    private modalCtrl: ModalController,
-    private menuCtrl: MenuController,
-    private storage: Storage) {
-  }
+    constructor(private navCtrl: NavController,
+        private roomsService: RoomService,
+        private modalCtrl: ModalController,
+        private menuCtrl: MenuController,
+        private storage: Storage,
+        private alertCtrl: AlertController) {
+    }
 
-  ngOnInit() {
+    ionViewWillEnter() {
+        this.getFavoriteRooms();
+    }
 
-  }
+    goToRoomDetailPage(selectedRoomId) {
+        this.navCtrl.push(RoomPage, { 'roomId': selectedRoomId });
+    }
 
-  onGoToRoomDetailPage(selectedRoom: Room) {
-    this.navCtrl.push(RoomPage, selectedRoom);
-  }
+    removeFromFavorites(selectedRoomId) {
+        this.storage.get('favorites').then(val => {
+            var favoriteList: string[];
 
-  RefreshData() {
-    // this.rooms = this.roomsService.getFavoriteRooms();
-  }
+            if (val == undefined || val == null) {
+                favoriteList = [];
+            } else {
+                favoriteList = <string[]>val;
+            }
 
-  onRemoveFromFavorites(room: Room) {
-    // this.roomsService.removeRoomFromFavorites(room);
-    // this.RefreshData();
-  }
+            favoriteList.splice(favoriteList.indexOf(selectedRoomId), 1);
+            this.storage.set('favorites', favoriteList);
+
+        }).then(() => {
+            this.refreshData();
+        })
+    }
+
+    refreshData() {
+        this.getFavoriteRooms();
+    }
+
+    getFavoriteRooms() {
+        this.storage.get('favorites').then(val => {
+            var favoriteRoomIds: string[];
+
+            if (val !== undefined || val !== null) {
+
+                favoriteRoomIds = <string[]>val;
+
+                this.roomsService.getFavoriteRooms(favoriteRoomIds).then(res => {
+                    this.favoriteRooms = res;
+                    console.log(this.favoriteRooms);
+                }).catch(error => {
+
+                    var alert = this.alertCtrl.create({
+                        message: error,
+                        buttons: [{
+                            text: '確定',
+                        }]
+                    });
+
+                    alert.present();
+                })
+            }
+        });
+    }
 
 }
--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -4,88 +4,106 @@ import { RoomType } from '../model/roomType';
 import { Injectable } from "@angular/core";
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+//RxJS is a third party library, endorsed by Angular, that implements the asynchronous Observable pattern.
 
 @Injectable()
 export class RoomService {
 
-  private searchText: string;
-  private roomID: string;
+    private searchText: string;
+    private roomID: string;
 
-  constructor(
-    private http: Http) {
-  }
+    constructor(
+        private http: Http) {
+    }
 
-  getRoomTypes(): Promise<RoomType[]> {
-    return new Promise((resolve, reject) => {
-      this.http.get(AppSettings.apiHost + 'api/roomTypes')
-        .map(res => res.json())
-        .subscribe((response) => {
-          resolve(response.data);
-        }, (error) => {
-          reject(error);
+    getRoomTypes(): Promise<RoomType[]> {
+        return new Promise((resolve, reject) => {
+            this.http.get(AppSettings.apiHost + 'api/roomTypes')
+                .map(res => res.json())
+                .subscribe((response) => {
+                    resolve(response.data);
+                }, (error) => {
+                    reject(error);
+                });
         });
-    });
-  }
+    }
 
-  getDistricts(): Promise<District[]> {
-    return new Promise((resolve, reject) => {
-      this.http.get(AppSettings.apiHost + 'api/districts')
-        .map(res => res.json())
-        .subscribe((response) => {
-          resolve(response.data);
-        }, (error) => {
-          reject(error);
+    getDistricts(): Promise<District[]> {
+        return new Promise((resolve, reject) => {
+            this.http.get(AppSettings.apiHost + 'api/districts')
+                .map(res => res.json())
+                .subscribe((response) => {
+                    resolve(response.data);
+                }, (error) => {
+                    reject(error);
+                });
         });
-    });
-  }
+    }
 
-  searchRoom(roomId): Promise<any> {
+    searchRoom(roomId): Promise<any> {
 
-    console.log(AppSettings.apiHost + 'api/rooms/' + roomId);
+        console.log(AppSettings.apiHost + 'api/rooms/' + roomId);
 
-    return new Promise((resolve, reject) => {
-      this.http.get(AppSettings.apiHost + 'api/rooms/' + roomId)
-        .map(res => res.json())
-        .subscribe((response) => {
+        return new Promise((resolve, reject) => {
+            this.http.get(AppSettings.apiHost + 'api/rooms/' + roomId)
+                .map(res => res.json())
+                .subscribe((response) => {
 
-          resolve(JSON.parse(response.data));
+                    resolve(JSON.parse(response.data));
 
-        }, (error) => {
-          reject(error);
+                }, (error) => {
+                    reject(error);
+                });
         });
-    });
 
-  }
+    }
 
-  searchRooms(searchCriterias): Promise<any[]> {
+    searchRooms(searchCriterias): Promise<any[]> {
 
-    return new Promise((resolve, reject) => {
-      this.http.post(AppSettings.apiHost + 'api/rooms', searchCriterias)
-        .map(res => res.json())
-        .subscribe((response) => {
+        return new Promise((resolve, reject) => {
+            this.http.post(AppSettings.apiHost + 'api/rooms', searchCriterias)
+                .map(res => res.json())
+                .subscribe((response) => {
 
-          resolve(JSON.parse(response.data));
+                    resolve(JSON.parse(response.data));
 
-        }, (error) => {
-          reject(error);
+                }, (error) => {
+                    reject(error);
+                });
         });
-    });
 
-  }
+    }
 
-  // reserveRoom(data) {
+    getFavoriteRooms(favoriteRoomIds): Promise<any[]> {
+      console.log(favoriteRoomIds);
+        return new Promise((resolve, reject) => {
+            this.http.post(AppSettings.apiHost + 'api/favoriteRooms', favoriteRoomIds)
+                .map(res => res.json())
+                .subscribe((response) => {
 
-  //   return new Promise(resolve => {
+                    console.log(response);
 
-  //     let headers = new Headers();
-  //     headers.append('Content-Type', 'application/json');
+                    resolve(JSON.parse(response.data));
 
-  //     this.http.post('http://localhost:8080/api/rooms/reserve', JSON.stringify(data), { headers: headers })
-  //       .subscribe((data) => {
-  //         resolve(data);
-  //       });
+                }, (error) => {
+                    reject(error);
+                });
+        })
+    }
 
-  //   });
-  // }
+    // reserveRoom(data) {
+
+    //   return new Promise(resolve => {
+
+    //     let headers = new Headers();
+    //     headers.append('Content-Type', 'application/json');
+
+    //     this.http.post('http://localhost:8080/api/rooms/reserve', JSON.stringify(data), { headers: headers })
+    //       .subscribe((data) => {
+    //         resolve(data);
+    //       });
+
+    //   });
+    // }
 
 }
